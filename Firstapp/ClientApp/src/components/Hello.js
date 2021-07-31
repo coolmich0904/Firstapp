7801228f6506d5ee115f362bfd71037c8ea45b28
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-07-31 02:49:21 -0400

    Add project files.

---
+++ Firstapp/ClientApp/src/components/Hello.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Test </h1>
+                <h2>auto complete</h2>
+            </div>
+            )
+    }
+}
+
+export default Hello
